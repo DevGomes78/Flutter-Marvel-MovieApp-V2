2b340490ad82b891b9de3c063a686199024d3fb5
amilson gomes <gomes.amilson@gmail.com>
2022-08-23 14:49:33 -0300

    refatorado arquivo details_page.dart

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -60,39 +60,41 @@ class _DetailsPageState extends State<DetailsPage> {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(widget.data!.overview.toString()),
             ),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Container(
-                height: 60,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                    color: Colors.black54),
-                child: Row(
-                  children: [
-                    Text(
-                      'Assistir mais tarde',
-                      style: AppTextStyle.font22,
-                    ),
-                    Consumer<MyList>(
-                      builder: (context, value, child) => IconButton(
-                        onPressed: () {
-                          value.listLaterOnly(widget.data!);
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text('Assistir mais tarde!')));
-                        },
-                        icon: const Icon(
-                          Icons.add,
-                          size: 35,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
+            myList()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding myList() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40), color: Colors.black54),
+        child: Row(
+          children: [
+            Text(
+              'Assistir mais tarde',
+              style: AppTextStyle.font22,
+            ),
+            Consumer<MyList>(
+              builder: (context, value, child) => IconButton(
+                onPressed: () {
+                  value.listLaterOnly(widget.data!);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Assistir mais tarde!')));
+                },
+                icon: const Icon(
+                  Icons.add,
+                  size: 35,
+                  color: Colors.white,
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -101,116 +103,118 @@ class _DetailsPageState extends State<DetailsPage> {
 
   Row infoDetailsMovie() {
     return Row(
-            children: [
-              const SizedBox(width: 110),
-              Text(
-                (DateFormat("yyyy").format(
-                    DateTime.parse(widget.data!.releaseDate.toString()))),
-                style: const TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(width: 15),
-              container(),
-              const SizedBox(width: 15),
-              Text(
-                widget.data!.duration.toString(),
-                style: const TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(width: 5),
-              Text(
-                StringConstants.minutes,
-                style: AppTextStyle.font15,
-              ),
-              const SizedBox(width: 8),
-              container(),
-              const SizedBox(width: 18),
-              Text(
-                StringConstants.genre,
-                style: AppTextStyle.font15,
-              ),
-            ],
-          );
+      children: [
+        const SizedBox(width: 110),
+        Text(
+          (DateFormat("yyyy")
+              .format(DateTime.parse(widget.data!.releaseDate.toString()))),
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(width: 15),
+        container(),
+        const SizedBox(width: 15),
+        Text(
+          widget.data!.duration.toString(),
+          style: const TextStyle(
+            fontSize: 15,
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(width: 5),
+        Text(
+          StringConstants.minutes,
+          style: AppTextStyle.font15,
+        ),
+        const SizedBox(width: 8),
+        container(),
+        const SizedBox(width: 18),
+        Text(
+          StringConstants.genre,
+          style: AppTextStyle.font15,
+        ),
+      ],
+    );
   }
 
   Stack cardMovie() {
     return Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height / 2,
-                width: double.infinity,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40),
-                  ),
-                  child: Image.network(
-                    widget.data!.coverUrl.toString(),
-                    fit: BoxFit.fill,
-                  ),
-                ),
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: double.infinity,
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(40),
+              bottomRight: Radius.circular(40),
+            ),
+            child: Image.network(
+              widget.data!.coverUrl.toString(),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 15,
+          top: 15,
+          child: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, Routes.marvelListPage2);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          left: 170,
+          top: 100,
+          child: InkWell(
+            onTap: callVideoPlayer,
+            child: const Icon(
+              Icons.play_circle_outline,
+              color: Colors.yellow,
+              size: 65,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 20,
+          top: 20,
+          child: Consumer<Favorites>(
+            builder: (context, provider, child) => IconButton(
+              onPressed: () {
+                provider.toogleFavorite();
+                favoritesController(provider, context);
+              },
+              icon: Icon(
+                provider.listFavorites.contains(widget.data)
+                    ? Icons.favorite
+                    : Icons.favorite_border,
+                size: 30,
+                color: Colors.white,
               ),
-              Positioned(
-                left: 15,
-                top: 15,
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.marvelListPage2);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 170,
-                top: 100,
-                child: InkWell(
-                  onTap: callVideoPlayer,
-                  child: const Icon(
-                    Icons.play_circle_outline,
-                    color: Colors.yellow,
-                    size: 65,
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 20,
-                top: 20,
-                child: Consumer<Favorites>(
-                  builder: (context, provider, child) => IconButton(
-                    onPressed: () {
-                      provider.toogleFavorite();
-                      if (!provider.listFavorites.contains(widget.data)) {
-                        provider.listFavorites.add(widget.data!);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Adicionado aos Favoritos!')));
-                      } else {
-                        provider.listFavorites.remove(widget.data!);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('removido dos Favoritos!')));
-                      }
-                    },
-                    icon: Icon(
-                      provider.listFavorites.contains(widget.data)
-                          ? Icons.favorite
-                          : Icons.favorite_border,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          );
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void favoritesController(Favorites provider, BuildContext context) {
+    if (!provider.listFavorites.contains(widget.data)) {
+      provider.listFavorites.add(widget.data!);
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Adicionado aos Favoritos!')));
+    } else {
+      provider.listFavorites.remove(widget.data!);
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('removido dos Favoritos!')));
+    }
   }
 
   Container container() {
